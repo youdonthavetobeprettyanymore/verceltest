c4e16e8ca118346575642e6cfb87d4712c1b402f
youdonthavetobeprettyanymore <youdonthavetobeprettyanymore@gmail.com>
2024-12-15 16:53:54 -0800

    remove "posts" fetching that was no longer needed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,40 +2,13 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { type SanityDocument } from "next-sanity";
-import { client } from "./sanity/client";
 
 export const metadata = {
   title: "DAW",
   description: "DAW Homepage",
 };
 
-const POSTS_QUERY = `*[
-  _type == "event"
-  && defined(slug.current)
-]|order(eventDate desc)[0...12]{
-  _id,
-  title,
-  slug,
-  eventLineup,
-  eventDate,
-  location,
-  eventName,
-  images[]{
-    _key,
-    asset->{
-      _id,
-      url
-    }
-  }
-}`;
-
-const options = { next: { revalidate: 30 } };
-
-export default async function IndexPage() {
-  // Fetch posts if needed
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
-
+export default function IndexPage() {
   return (
     <main className="container mx-auto max-w-[1200px] p-8 flex flex-col items-center justify-center min-h-screen">
       {/* DAW Logo */}
